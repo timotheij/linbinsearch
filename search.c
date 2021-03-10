438c4bc2a108600a1d2c1b8210bbff2b9bdef25e
timotheij <timothei.penders@outlook.com>
2021-03-10 21:31:46 +0100

    Modify binsearch.c to add binary search

--- search.c
+++ search.c
@@ -10,7 +10,16 @@ size_t linsearch(uint8_t const *array, size_t length, uint8_t target)
 	return length+1; // no match
 }
 
-size_t binsearch(uint8_t *array, size_t length, uint8_t target)
+size_t binsearch(uint8_t const *array, size_t length, uint8_t target)
 {
-	return 0;
+	size_t min = 0;
+	size_t max = length - 1;
+
+	while (max >= min) {
+		uint8_t guess = array[(min + max) / 2];
+		if (guess == target) return guess;
+		else if (guess < target) min = guess + 1;
+		else if (guess > target) max = guess - 1;
+	}
+	return length+1; // no match
 }
--- search.h
+++ search.h
@@ -2,4 +2,4 @@
 
 size_t linsearch(uint8_t const *array, size_t length, uint8_t target);
 
-size_t binsearch(uint8_t *array, size_t length, uint8_t target);
+size_t binsearch(uint8_t const *array, size_t length, uint8_t target);
